3f53acf60e1a84a7bb86934cccd79ca214699f0a
Poochas <139040897+Poochas@users.noreply.github.com>
2023-08-12 18:24:06 +0400

    Added play round Added logic to play one round of game

--- script.js
+++ script.js
@@ -7,5 +7,25 @@ function getComputerChoice() {
     //get random array item
     return arr[randomIndex];
 }
-
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice()
+const playerSelection = prompt("Rock/Paper/Scissors ?")
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toUpperCase()
+    const computer = computerSelection.toUpperCase()
+    if (player === computer) {
+        return (`Tie! ${playerSelection} is equal to ${computerSelection}`)
+    }
+    else if (player === 'ROCK' && computer === 'SCISSORS') {
+        return (`You win ! ${playerSelection} beats ${computerSelection}`)
+    }
+    else if (player === 'PAPER' && computer === 'ROCK') {
+        return (`You win ! ${playerSelection} beats ${computerSelection}`)
+    }
+    else if (player === 'SCISSORS' && computer === 'PAPER') {
+        return (`You win ! ${playerSelection} beats ${computerSelection}`)
+    }
+    else {
+        return (`You lost ! ${computerSelection} beats ${playerSelection}`)
+    }
+}
+console.log(playRound(playerSelection, computerSelection))
