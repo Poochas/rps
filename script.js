52a68c9cc787e7a4abb90adc2e9d5c85c1ccd3c2
Poochas <139040897+Poochas@users.noreply.github.com>
2023-08-27 16:03:44 +0400

    Final design

--- script.js
+++ script.js
@@ -1,191 +1,148 @@
 hide('retry')
 const buttons = document.querySelectorAll('i');
-const playerScore = document.querySelector('#playerScore');
-const computerScore = document.querySelector('#computerScore');
-const round = document.querySelector('.round');
-const winner = document.querySelector('#winner');
-
-let computerScoreValue = 0
-let playerScoreValue = 0
-let currentRound = 1
-
-computerScore.innerText = computerScoreValue
-playerScore.innerText = playerScoreValue
-
-// computerScore.innerText = parseInt(computerScore.innerText) + 1
-// we use the .forEach method to iterate through each button
-// function restart() {
-
-//     let playerScore = 0;
-//     let computerScore = 0;
-// }
+const currentRound = document.getElementById('currentRound');
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
+let gameOver = false;
+let playerSelectedIcon;
+let computerSelectedIcon;
 
 buttons.forEach((button) => {
-    // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
-        if (button.id === 'Retry') {
-            currentRound = 1
-            computerScoreValue = 0
-            playerScoreValue = 0
-            hide('retry');
-            show('icons');
-            restart();
-            round.innerText = 'Round: 1'
-            computerScore.innerText = computerScoreValue
-            playerScore.innerText = playerScoreValue
-            winner.innerText = 'Choose your weapon'
+        if (button.parentNode.id === 'playerIcons' && (!gameOver)) {
+            setSelection(button.id, button.parentNode.id)
+            addLog(playRound(button.id), false)
+            gameOver = game()
         }
-        else {
-            setSelection(button)
-            currentRound++
-            showChoice('player', button.id)
-            round.innerText = `Round: ${currentRound}`
-            game(playRound(button.id), currentRound)
-        }
-    });
-});
+        if (button.id === 'retry') { restart() }
+    })
+})
 
-function playRound(playerSelection) {
-    const player = playerSelection.toUpperCase()
-    const computer = getComputerChoice().toUpperCase()
-    if (player === computer) {
-        return 'Tie'
-    }
-    else if (player === 'ROCK' && computer === 'SCISSORS') {
-        return 'Player'
-    }
-    else if (player === 'PAPER' && computer === 'ROCK') {
-        return 'Player'
-    }
-    else if (player === 'SCISSORS' && computer === 'PAPER') {
-        return 'Player'
-    }
-    else {
-        return 'Computer'
-    }
+function restart() {
+    if (document.getElementsByClassName('winner')[0] != null) { document.getElementsByClassName('winner')[0].classList.remove('winner') }
+    if (document.getElementsByClassName('loser')[0] != null) { document.getElementsByClassName('loser')[0].classList.remove('loser') }
+    currentRound.innerText = '1'
+    playerScore.innerText = '0'
+    computerScore.innerText = '0'
+    setSelection('', 'playerIcons')
+    setSelection('', 'computerIcons')
+    document.getElementById('log').innerHTML = ''
+    hide('retry')
+    gameOver = false;
 }
 
-// function to get a random item from an array
+function setSelection(button, player) {
+    buttons.forEach((item) => {
+        if (item.parentNode.id === player) {
+            item.classList.replace('fa-solid', 'fa-regular')
+            if (item.id === button) {
+                item.classList.replace('fa-regular', 'fa-solid');
+                if (player === 'computerIcons') {
+                    computerSelectedIcon = item;
+                } else { playerSelectedIcon = item }
+            }
+        }
+    })
+}
 
 function getComputerChoice() {
     const arr = ['rock', 'paper', 'scissors'];
     // get random index value
     const randomIndex = Math.floor(Math.random() * arr.length);
-    showChoice('computer', arr[randomIndex])
+    setSelection(arr[randomIndex], 'computerIcons')
     //get random array item
     return arr[randomIndex];
 }
 
-function game(roundWinner, currentRound) {
-    computerScoreValue = computerScore.innerText
-    playerScoreValue = playerScore.innerText
-    addLog(roundWinner);
-    if (roundWinner === 'Player') {
+function playRound(playerSelection) {
+    const player = playerSelection.toUpperCase()
+    const computer = getComputerChoice().toUpperCase()
+
+    let computerScoreValue = parseInt(computerScore.innerText);
+    let playerScoreValue = parseInt(playerScore.innerText);
+    if (player === computer) {
+        return 'Tie'
+    }
+    else if (player === 'ROCK' && computer === 'SCISSORS') {
         playerScoreValue++
         playerScore.innerText = playerScoreValue
-        winner.innerText = `${roundWinner} wins this round`
-        setWinner('player');
-        setLoser('computer');
-
+        return 'Player'
     }
-    else if (roundWinner === 'Computer') {
-        computerScoreValue++
-        computerScore.innerText = computerScoreValue
-        winner.innerText = `${roundWinner} wins this round`
-        setWinner('computer');
-        setLoser('player');
-        // addLog('Computer')
+    else if (player === 'PAPER' && computer === 'ROCK') {
+        playerScoreValue++
+        playerScore.innerText = playerScoreValue
+        return 'Player'
+    }
+    else if (player === 'SCISSORS' && computer === 'PAPER') {
+        playerScoreValue++
+        playerScore.innerText = playerScoreValue
+        return 'Player'
     }
     else {
-        winner.innerText = 'This round is tie!'
-        setTie();
-    }
-
-    if (currentRound == 6) {
-        if (playerScoreValue === computerScoreValue) {
-            round.innerText = 'Game is tie !';
-            winner.innerText = '';
-            hide('icons');
-            show('retry');
-
-            // round.innerText = 'It`s a Tie !'
-        } else if (playerScoreValue > computerScoreValue) {
-            round.innerText = 'You won the game !';
-            winner.innerText = '';
-            hide('icons');
-            show('retry');
-            // round.innerText = 'You win the game !';
-        }
-        else {
-            round.innerText = 'You lost the game !';
-            winner.innerText = '';
-            hide('icons');
-            show('retry');
-        }
+        computerScoreValue++
+        computerScore.innerText = computerScoreValue
+        return 'Computer'
     }
 }
 
 function hide(item) {
     const hide = document.getElementById(item);
-    // hide.style.visibility = 'hidden';
-    hide.style.display = 'none';
+    hide.style.visibility = 'hidden';
 }
 function show(item) {
     const show = document.getElementById(item);
-    show.style.display = 'flex';
-    // show.style.visibility = 'visible';
+    show.style.visibility = 'visible';
 }
 
-function showChoice(player, choice) {
-    const element = document.querySelector(`#${player}Selection`);
-    switch (choice) {
-        case 'rock':
-            element.className = 'fa-solid fa-hand-back-fist fa-2x';
-            element.style.visibility = 'visible';
-            break;
-        case 'paper':
-            element.className = 'fa-solid fa-hand fa-2x';
-            element.style.visibility = 'visible';
-            break;
-        case 'scissors':
-            element.className = 'fa-solid fa-hand-scissors fa-rotate-90 fa-2x';
-            element.style.visibility = 'visible';
-            break;
+function addLog(winner, game) {
+    element = document.createElement('li');
+    if (!game) {
+        const elementText = winner.toUpperCase() === 'TIE' ? 'This round is tie !' : `${winner} wins this round !`
+        element.innerText = elementText
+    } else {
+        const elementText = winner.toUpperCase() === 'TIE' ? 'This game is tie !' : `${winner} wins the game !`
+        element.innerText = elementText
+        element.classList.add('endGame')
+    }
+    document.getElementById('log').appendChild(element);
+}
+
+
+function game() {
+    let computerScoreValue = parseInt(computerScore.innerText);
+    let playerScoreValue = parseInt(playerScore.innerText);
+    let currentRoundValue = parseInt(currentRound.innerText);
+    currentRoundValue++
+
+    if (currentRoundValue === 6) {
+        if (playerScoreValue === computerScoreValue) {
+            addLog('Tie', true);
+        }
+        else if (playerScoreValue > computerScoreValue) {
+            addLog('Player', true);
+            setWinner('player');
+            setLoser('computer');
+        }
+        else {
+            addLog('Computer', true);
+            setWinner('computer');
+            setLoser('player');
+        }
+        currentRound.innerText = currentRoundValue - 1
+        show('retry')
+        return true;
+    }
+    else {
+        currentRound.innerText = currentRoundValue
+        return false;
     }
 }
 
 function setWinner(player) {
-    const element = document.querySelector(`#${player}Selection`);
-    element.classList.add('winner')
-
+    const element = document.getElementsByClassName(player);
+    element[0].firstElementChild.classList.add('winner');
 }
 function setLoser(player) {
-    const element = document.querySelector(`#${player}Selection`);
-    element.classList.add('loser')
-    element.classList.add('fa-fade')
-}
-
-function setTie() {
-    playerSelection.classList.add('tie');
-    computerSelection.classList.add('tie');
-}
-
-function setSelection(button) {
-    buttons.forEach((item) => {
-        console.log(item.id)
-        if (item.id != 'Retry') { item.classList.replace('fa-solid', 'fa-regular') }
-    })
-    button.classList.replace('fa-regular', 'fa-solid')
-}
-
-function addLog(winner) {
-    element = document.createElement('li');
-    const elementText = winner.toUpperCase() === 'TIE' ? 'This round is tie !' : `${winner} wins this round !`
-    element.innerText = elementText
-    document.getElementById('log').appendChild(element);
-}
-
-function restart() {
-    const ul = document.getElementById('log');
-    ul.innerHTML = '';
+    const element = document.getElementsByClassName(player);
+    element[0].firstElementChild.classList.add('loser');
 }
